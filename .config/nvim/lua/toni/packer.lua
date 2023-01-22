05f66d66affd0589a41fe7bf3e1b0f51cd9f7d55
tonivegaochoa <tonivegaochoa@gmail.com>
2023-01-22 15:08:24 -0500

    Added lsp and indent-blankline
    Added lsp with lsp-zero package
    Added package for showing blankline characters

--- .config/nvim/lua/toni/packer.lua
+++ .config/nvim/lua/toni/packer.lua
@@ -1,5 +1,3 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
@@ -9,7 +7,6 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
@@ -31,4 +28,29 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-tree.lua'
 
   use 'lewis6991/gitsigns.nvim'
+
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v1.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {'williamboman/mason.nvim'},           -- Optional
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},         -- Required
+    {'hrsh7th/cmp-nvim-lsp'},     -- Required
+    {'hrsh7th/cmp-buffer'},       -- Optional
+    {'hrsh7th/cmp-path'},         -- Optional
+    {'saadparwaiz1/cmp_luasnip'}, -- Optional
+    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+    -- Snippets
+    {'L3MON4D3/LuaSnip'},             -- Required
+    {'rafamadriz/friendly-snippets'}, -- Optional
+  }
+}
 end)
