f9c12057239ac50400b10eee81892ef27b01ffa9
tonivegaochoa <tonivegaochoa@gmail.com>
2025-08-18 08:08:14 -0500

    update aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,4 +4,15 @@ alias deeplink='xcrun simctl openurl booted'
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias erc='nvim ~/.zshrc'
 alias src='source ~/.zshrc;echo "ZSH sourced."'
-alias fov='nvim $(fzf --preview "bat --color=always --style=numbers --line-range=:500 {}")'
+alias fov='f=$(fd --type f --hidden --ignore-file=$HOME/.ignore \
+  | fzf --preview "bat --color=always --style=numbers --line-range=:500 {}") && [ -n "$f" ] && nvim "$f"'
+alias ls='eza'
+
+# git aliases
+alias gap='git add -p'
+alias gst='git status --ignore-submodules=all'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias grb='git rebase'
+alias gri='git rebase -i'
+alias gcpr='git rev-parse HEAD | pbcopy'
