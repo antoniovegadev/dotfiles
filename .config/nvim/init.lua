7bc4f2eb447bfae31c20cc82e91299b36ee42cd5
tonivegaochoa <tonivegaochoa@gmail.com>
2025-08-21 16:06:16 -0500

    update nvim-treesitter config to use 'main' branch api

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,29 @@
 require("av.options")
 require("av.keymaps")
 require("av.plugins")
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    "astro",
+    "css",
+    "json",
+    "lua",
+    "markdown_inline",
+    "markdown",
+    "ruby",
+    "swift",
+    "terraform",
+    "toml",
+    "typescript",
+    "vim",
+    "vimdoc",
+    "yaml",
+  },
+  callback = function()
+    vim.treesitter.start()
+    vim.wo.foldmethod = "expr"
+    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    vim.opt.foldlevel = 99
+  end,
+})
--- .config/nvim/lua/av/plugins/treesitter.lua
+++ .config/nvim/lua/av/plugins/treesitter.lua
@@ -1,23 +1,18 @@
-require("nvim-treesitter").setup({
-  ensure_installed = {
-    "astro",
-    "css",
-    "json",
-    "lua",
-    "markdown_inline",
-    "markdown",
-    "ruby",
-    "swift",
-    "terraform",
-    "toml",
-    "typescript",
-    "vim",
-    "vimdoc",
-    "yaml",
-  },
-  indent = { enable = true },
-  auto_install = false,
-  highlight = {
-    enable = true,
-  },
+local ts = require("nvim-treesitter")
+
+ts.install({
+  "astro",
+  "css",
+  "json",
+  "lua",
+  "markdown_inline",
+  "markdown",
+  "ruby",
+  "swift",
+  "terraform",
+  "toml",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "yaml",
 })
